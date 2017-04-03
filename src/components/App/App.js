1d18f1ab8f9905316b651c5213d85747dbf9d486
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2017-04-03 20:49:06 -0300

    testando react com daniel

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Input from '../input/Input.js';
 
 export default class App extends React.Component {
   render() {
     return (
      <div style={{textAlign: 'center'}}>
-        <h1>Hello World 3 </h1>
+        <h1>Hello World 30 </h1>
         .<Input />.
         <hr />
       </div>);
