a87d18e297ba40487cb16607c5cf9f1cdc1193af
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2017-04-05 13:03:42 -0300

    passando de 2 para 4 espaços

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,12 +4,13 @@ import Input from '../input/Input.js';
 require('./app.scss');
 
 export default class App extends React.Component {
-  render() {
-    return (
-     <div style={{textAlign: 'center'}} className="module-app">
-        <h1>Hello World 30 </h1>
-        <Input />
-        <hr />
-      </div>);
-  }
+    render() {
+        return (
+            <div style={{textAlign: 'center'}} className="module-app">
+                <h1>Hello World 30</h1>
+                <Input />
+                <hr />
+            </div>
+        );
+    }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -7,145 +7,145 @@ const staticsPath = path.join(__dirname, './static');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = function (env) {
-  const nodeEnv = env && env.prod ? 'production' : 'development';
-  const isProd = nodeEnv === 'production';
+    const nodeEnv = env && env.prod ? 'production' : 'development';
+    const isProd = nodeEnv === 'production';
 
-  const plugins = [
-    new webpack.optimize.CommonsChunkPlugin({
-      name: 'vendor',
-      minChunks: Infinity,
-      filename: 'vendor.bundle.js'
-    }),
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: nodeEnv,
-    }),
-    new webpack.NamedModulesPlugin(),
-    new ExtractTextPlugin('static/styles.css')
-  ];
+    const plugins = [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: Infinity,
+            filename: 'vendor.bundle.js'
+        }),
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: nodeEnv,
+        }),
+        new webpack.NamedModulesPlugin(),
+        new ExtractTextPlugin('static/styles.css')
+    ];
 
-  var sassUse;
+    var sassUse;
 
-  if (isProd) {
-    plugins.push(
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      }),
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: false,
-          screw_ie8: true,
-          conditionals: true,
-          unused: true,
-          comparisons: true,
-          sequences: true,
-          dead_code: true,
-          evaluate: true,
-          if_return: true,
-          join_vars: true,
+    if (isProd) {
+        plugins.push(
+            new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false
+            }),
+            new webpack.optimize.UglifyJsPlugin({
+                compress: {
+                    warnings: false,
+                    screw_ie8: true,
+                    conditionals: true,
+                    unused: true,
+                    comparisons: true,
+                    sequences: true,
+                    dead_code: true,
+                    evaluate: true,
+                    if_return: true,
+                    join_vars: true,
+                },
+                output: {
+                    comments: false,
+                },
+            })
+        );
+
+        sassUse = ExtractTextPlugin.extract({
+        fallback: 'style-loader', // The backup style loader
+        use: 'css-loader!sass-loader'
+        });
+    } else {
+        plugins.push(
+            new webpack.HotModuleReplacementPlugin()
+        );
+
+        sassUse = [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+        ];
+    }
+
+    return {
+        devtool: isProd ? 'source-map' : 'eval',
+        context: sourcePath,
+        entry: {
+            js: './index.js',
+            vendor: ['react']
         },
         output: {
-          comments: false,
+            path: staticsPath,
+            filename: '[name].bundle.js',
         },
-      })
-    );
+        module: {
+            rules: [
+                {
+                    test: /\.html$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'file-loader',
+                        query: {
+                            name: '[name].[ext]'
+                        },
+                    },
+                },
+                {
+                    test: /\.scss$/,
+                    exclude: /node_modules/,
+                    use: sassUse
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    use: [
+                        'babel-loader'
+                    ],
+                },
+            ],
+        },
+        resolve: {
+            extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
+            modules: [
+                path.resolve(__dirname, 'node_modules'),
+                sourcePath
+            ]
+        },
 
-    sassUse = ExtractTextPlugin.extract({
-      fallback: 'style-loader', // The backup style loader
-      use: 'css-loader!sass-loader'
-    });
-  } else {
-    plugins.push(
-      new webpack.HotModuleReplacementPlugin()
-    );
+        plugins,
 
-    sassUse = [
-      'style-loader',
-      'css-loader',
-      'sass-loader'
-    ];
-  }
+        performance: isProd && {
+            maxAssetSize: 100,
+            maxEntrypointSize: 300,
+            hints: 'warning',
+        },
 
-  return {
-    devtool: isProd ? 'source-map' : 'eval',
-    context: sourcePath,
-    entry: {
-      js: './index.js',
-      vendor: ['react']
-    },
-    output: {
-      path: staticsPath,
-      filename: '[name].bundle.js',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.html$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'file-loader',
-            query: {
-              name: '[name].[ext]'
+        stats: {
+            colors: {
+                green: '\u001b[32m',
+            }
+        },
+
+        devServer: {
+            contentBase: './public',
+            historyApiFallback: true,
+            port: 3000,
+            compress: isProd,
+            inline: !isProd,
+            hot: !isProd,
+            stats: {
+                assets: true,
+                children: false,
+                chunks: false,
+                hash: false,
+                modules: false,
+                publicPath: false,
+                timings: true,
+                version: false,
+                warnings: true,
+                colors: {
+                    green: '\u001b[32m',
+                }
             },
-          },
-        },
-        {
-          test: /\.scss$/,
-          exclude: /node_modules/,
-          use: sassUse
-        },
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: [
-            'babel-loader'
-          ],
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.webpack-loader.js', '.web-loader.js', '.loader.js', '.js', '.jsx'],
-      modules: [
-        path.resolve(__dirname, 'node_modules'),
-        sourcePath
-      ]
-    },
-
-    plugins,
-
-    performance: isProd && {
-      maxAssetSize: 100,
-      maxEntrypointSize: 300,
-      hints: 'warning',
-    },
-
-    stats: {
-      colors: {
-        green: '\u001b[32m',
-      }
-    },
-
-    devServer: {
-      contentBase: './public',
-      historyApiFallback: true,
-      port: 3000,
-      compress: isProd,
-      inline: !isProd,
-      hot: !isProd,
-      stats: {
-        assets: true,
-        children: false,
-        chunks: false,
-        hash: false,
-        modules: false,
-        publicPath: false,
-        timings: true,
-        version: false,
-        warnings: true,
-        colors: {
-          green: '\u001b[32m',
         }
-      },
-    }
-  };
+    };
 };
