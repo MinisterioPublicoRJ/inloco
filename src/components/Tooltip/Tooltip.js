07b6e6a3dd051773fc3c4abbebc6e4f023752ab0
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-04-12 17:07:27 -0300

    Adding tooltip component

---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+/**
+ * Tooltip compoment that explains or describe another component
+ */
+
+export default class Tooltip extends React.Component {
+
+    /**
+     * Component constructor
+     */
+
+    constructor() {
+        super();
+    }
+
+    /**
+     * Renders component
+     * @return component's HTML
+     */
+
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}
