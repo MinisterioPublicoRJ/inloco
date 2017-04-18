4206ef26ca9eac34095d7a39d5b99cd7b601dba9
Arlindo Pereira <nighto@users.noreply.github.com>
2017-04-18 15:59:14 -0300

    removing unused jsdom on jest setup

--- jestsetup.js
+++ jestsetup.js
@@ -4,20 +4,6 @@ global.shallow = shallow;
 global.render = render;
 global.mount = mount;
 
-var jsdom = require('jsdom').jsdom;
-
-global.document = jsdom('');
-global.window = document.defaultView;
-Object.keys(document.defaultView).forEach((property) => {
-    if (typeof global[property] === 'undefined') {
-        global[property] = document.defaultView[property];
-    }
-});
-
-global.navigator = {
-    userAgent: 'node.js'
-};
-
 // Fail tests on any warning
 console.error = message => {
     throw new Error(message);
