439c3c5da006809762e204fac8496762f1b5ee6b
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-05-15 18:52:30 -0300

    Recursively rendering menu items

--- src/components/MenuItem/MenuItem.js
+++ src/components/MenuItem/MenuItem.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Menu from '../Menu/Menu';
-import SubMenu from '../SubMenu/SubMenu';
 
 const MenuItem = ({item, camadas}) => {
     console.log("item", item)
@@ -8,8 +7,8 @@ const MenuItem = ({item, camadas}) => {
     // debugger;
     return (
         <li>
-            { item.title ? item.title : camadas[item].title }
-            { item.camadas ? <SubMenu items={item.camadas} camadas={camadas} /> : '' }
+            { item.title ? item.title : camadas[item-1].title }
+            { item.camadas ? <Menu menu={item.camadas} camadas={camadas} /> : '' }
         </li>
     );
 }
