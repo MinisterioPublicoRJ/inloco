79b13844444a8e26c4a2e033964d4b6643bd1844
Barbier <gabrielnasb@gmail.com>
2017-05-24 14:34:10 -0300

    Search layer CSS

--- src/components/SearchLayer/SearchLayer.js
+++ src/components/SearchLayer/SearchLayer.js
@@ -3,13 +3,20 @@ import React from 'react';
 const SearchLayer = ({searchLayer}) => {
     let input;
     return (
-        <form action="#">
-            <input
-                type="text"
-                ref={node => {input = node;}}
-                onKeyUp={() => {
-                    searchLayer(input.value)
-                }}/>
+        <form action="#" className="search-layer">
+            <label htmlFor="searchLayer" className="search-layer--title">
+                Ou pesquise por aqui:
+                <span className="search-layer--icon fa fa-search"></span>
+                <input
+                    type="text"
+                    id="searchLayer"
+                    ref={node => {input = node;}}
+                    onKeyUp={() => {
+                        searchLayer(input.value)
+                    }}
+                    className="search-layer--input"
+                    placeholder="Ex.: IDEB, Escola, Batalhões da PMERJ"/>
+            </label>
         </form>
     );
 };
