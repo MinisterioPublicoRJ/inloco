6e3a625e10233ff3c56d2d400271499d87aebbf7
Barbier <gabrielnasb@gmail.com>
2017-05-24 16:12:47 -0300

    Sidebar title

--- src/components/SidebarLeft/SidebarLeft.js
+++ src/components/SidebarLeft/SidebarLeft.js
@@ -5,6 +5,11 @@ import SearchLayer from '../SearchLayer/SearchLayer.js';
 const SidebarLeft = ({searchLayer}) => {
     return (
         <div className="sidebar-left">
+            <img src="/src/assets/img/logo.png" alt="" className="sidebar-left--logo"/>
+            <h1 className="sidebar-left--title">
+                camadas de exibição
+                <small className="sidebar-left--caption">Escolha uma categoria abaixo</small>
+            </h1>
             <MenuContainer/>
             <SearchLayer searchLayer={searchLayer}/>
         </div>
