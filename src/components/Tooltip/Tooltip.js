f6cf86a2149a8da6d354120fb73f6daf9b746363
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-05-26 15:35:36 -0300

    Styling tooltip position using mouse x and y attributes

--- src/components/Tooltip/Tooltip.js
+++ src/components/Tooltip/Tooltip.js
@@ -3,14 +3,19 @@ import React from 'react';
 const Tooltip = ({tooltip}) => {
     var className = "";
     var text = "";
+    var divStyle = {};
     if(tooltip && tooltip.show){
         className = "tooltip";
         text = tooltip.text;
+        divStyle = {
+            left: tooltip.x,
+            top: tooltip.y
+        }
     }
     return (
         <div>
             {tooltip ?
-                <div className={className}>
+                <div style = {divStyle} className={className}>
                     { text }
                 </div>
             : ''
