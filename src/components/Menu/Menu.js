6241dbad884c936ec1c907bc80cfaaba038e73ec
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2017-05-31 19:39:19 -0300

    Showing submenu items, click doesn't work yet

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import MenuItem from './MenuItem'
 
-const Menu = ({menuItems, menuTitle, parentMenuTitle, layers, onLayerClick, onMenuItemClick, onMouseOver, sidebarLeftWidth, sidebarLeftHeight, onMouseOut, onUntoggleAllClick, selected, currentLevel}) => {
+const Menu = ({menuItems, menuTitle, menuSubMenu, parentMenuTitle, layers, onLayerClick, onMenuItemClick, onMouseOver, sidebarLeftWidth, sidebarLeftHeight, onMouseOut, onUntoggleAllClick, selected, currentLevel}) => {
 
     // add a selected class to the menu if it is selected
     let menuClassName = "menu menu-container" + (selected ? ' selected' : '')
@@ -22,6 +22,27 @@ const Menu = ({menuItems, menuTitle, parentMenuTitle, layers, onLayerClick, onMe
                 (!menuTitle && currentLevel > 0 && !itemsNotMatched) ?
                     <li className="menu-item-all-layers" onClick={onUntoggleAllClick}>Todas as camadas</li>
                 : ''
+            }
+            {
+                menuSubMenu ? menuSubMenu.map(
+                    (subMenu, idx) =>
+                    <MenuItem
+                        key={subMenu.idMenu}
+                        item={subMenu}
+                        layers={subMenu.layers}
+                        onLayerClick={onLayerClick}
+                        onMenuItemClick={onMenuItemClick}
+                        onItemClick={onMenuItemClick}
+                        onMouseOver={onMouseOver}
+                        sidebarLeftWidth={sidebarLeftWidth}
+                        sidebarLeftHeight={sidebarLeftHeight}
+                        onMouseOut={onMouseOut}
+                        parentMenuTitle={menuTitle}
+                        currentLevel={currentLevel}
+                        allMenuItems={menuItems}
+                        isSubMenu={true}
+                    />
+                ) : ''
             }
             {
                 menuItems ? menuItems.map(
--- src/components/Menu/MenuItem.js
+++ src/components/Menu/MenuItem.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Menu from './Menu'
 
-const MenuItem = ({item, layers, onItemClick, onMouseOver, sidebarLeftWidth, sidebarLeftHeight, onMouseOut, onMenuItemClick, onLayerClick, parentMenuTitle, currentLevel, allMenuItems}) => {
+const MenuItem = ({item, layers, onItemClick, onMouseOver, sidebarLeftWidth, sidebarLeftHeight, onMouseOut, onMenuItemClick, onLayerClick, parentMenuTitle, currentLevel, allMenuItems, isSubMenu}) => {
 
     // class name if menu item with children or single layer, with no children
     let menuItemClassName
@@ -10,58 +10,66 @@ const MenuItem = ({item, layers, onItemClick, onMouseOver, sidebarLeftWidth, sid
     let otherIsSelected = false
     let otherIsNotMatched = false
 
-    // if `item`, which means it is a submenu with children
-    if(item){
-        menuItemClassName = item.title ? 'menu-item-with-children' : 'menu-layer'
-        menuItemClassName += item.selected ? ' selected' : ''
+    if(!isSubMenu){
 
-        // check if one menu is selected, and no other items are matched, hide others
-        if (allMenuItems) {
-            for (let otherMenuItem of allMenuItems) {
-                if (otherMenuItem.title && otherMenuItem.title !== item.title) {
-                    if (otherMenuItem.selected) {
-                        otherIsSelected = true
-                    }
-                    if (!otherMenuItem.match) {
-                        otherIsNotMatched = true
+        // if `item`, which means it is a submenu with children
+        if(item){
+            menuItemClassName = item.title ? 'menu-item-with-children' : 'menu-layer'
+            menuItemClassName += item.selected ? ' selected' : ''
+
+            // check if one menu is selected, and no other items are matched, hide others
+            if (allMenuItems) {
+                for (let otherMenuItem of allMenuItems) {
+                    if (otherMenuItem.title && otherMenuItem.title !== item.title) {
+                        if (otherMenuItem.selected) {
+                            otherIsSelected = true
+                        }
+                        if (!otherMenuItem.match) {
+                            otherIsNotMatched = true
+                        }
                     }
                 }
-            }
-            if (otherIsSelected && !otherIsNotMatched) {
-                // we can only hide other items if not on search
-                visibleClass = 'hidden'
-            }
-        }
-
-        // if menu with children, and it doesn't match search, hide it
-        if (item.title) {
-            itemTitle = item.title
-            if (!item.match) {
-                visibleClass = 'hidden'
-            }
-        } else {
-            // if it's a layer, check if it's match'ed.
-            for (var i = 0 ; i < layers.length ; i++) {
-                var layer = layers[i]
-                if (layer.key === item) {
-                    itemTitle = layer.title
-                    if (!layer.match) {
-                        visibleClass = 'hidden'
-                    }
+                if (otherIsSelected && !otherIsNotMatched) {
+                    // we can only hide other items if not on search
+                    visibleClass = 'hidden'
                 }
             }
 
-            // check if layer is selected
-            if (!item.title){
+            // if menu with children, and it doesn't match search, hide it
+            if (item.title) {
+                itemTitle = item.title
+                if (!item.match) {
+                    visibleClass = 'hidden'
+                }
+            } else {
+                // if it's a layer, check if it's match'ed.
                 for (var i = 0 ; i < layers.length ; i++) {
                     var layer = layers[i]
-                    if (layer.key === item && layer.selected) {
-                        menuItemClassName += ' layer-selected';
+                    if (layer.key === item) {
+                        itemTitle = layer.title
+                        if (!layer.match) {
+                            visibleClass = 'hidden'
+                        }
+                    }
+                }
+
+                // check if layer is selected
+                if (!item.title){
+                    for (var i = 0 ; i < layers.length ; i++) {
+                        var layer = layers[i]
+                        if (layer.key === item && layer.selected) {
+                            menuItemClassName += ' layer-selected';
+                        }
                     }
                 }
             }
+            visibleClass += ' menu-item-container'
         }
-        visibleClass += " menu-item-container"
+    } else {
+        // is subMenu
+        itemTitle = item.title
+        visibleClass = 'menu-item-container'
+        menuItemClassName = 'menu-item-with-children'
     }
 
     return (
@@ -78,6 +86,7 @@ const MenuItem = ({item, layers, onItemClick, onMouseOver, sidebarLeftWidth, sid
                 (item && item.layers) ?
                 <Menu
                     menuItems={item.layers}
+                    menuSubMenu={item.subMenu}
                     menuTitle={item.title}
                     parentMenuTitle={parentMenuTitle}
                     key={item.idMenu}
