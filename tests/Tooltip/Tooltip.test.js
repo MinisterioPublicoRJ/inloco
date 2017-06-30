5bd4ba7cbe29d616db7779a4a77d647cc80806f5
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2017-06-30 16:09:04 -0300

    Tooltip test with real data
    Issue #140

--- tests/Tooltip/Tooltip.test.js
+++ tests/Tooltip/Tooltip.test.js
@@ -6,3 +6,12 @@ it('Tooltip component renders correctly with no data', () => {
     const component = shallow(<Tooltip/>);
     expect(component).toMatchSnapshot();
 });
+
+it('Tooltip component renders correctly with real data', () => {
+    let tooltip = {
+        "text":"Índice de Desenvolvimento da Educação Básica (IDEB) da rede estadual de educação. Leitura da tabela: I (Anos Iniciais), F (Anos Finais), E (Estadual), M (Municipal), P (Pública). Fonte: INEP Ano: 2015","show":true,"sidebarLeftWidth":300,"parentHeight":33,"top":230
+    }
+
+    const component = shallow(<Tooltip tooltip={tooltip}/>);
+    expect(component).toMatchSnapshot();
+});
