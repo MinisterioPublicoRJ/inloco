0108422d9ffc99f830e0436b983c18d8dc520ce0
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-06-30 15:53:54 -0300

    Drag feedbacks to user the position to drop

--- src/components/LayerSubtitle/LayerSubtitleSpace.js
+++ src/components/LayerSubtitle/LayerSubtitleSpace.js
@@ -1,18 +1,44 @@
 import React from 'react'
 import LayerSubtitle from '../LayerSubtitle/LayerSubtitle.js'
+import { DropTarget } from 'react-dnd'
 
-const LayerSubtitleSpace = ({ layer, id, onLayerClick, onLayerUp, onLayerDown, onLayerDrag , connectDragSource, isDragging}) => {
+const subtitleTarget = {
+    drop(props, monitor) {
+        console.log(props)
+    }
+}
 
-    return (
-        <LayerSubtitle
-            layer={layer}
-            key={id}
-            onLayerClick={onLayerClick}
-            onLayerUp={onLayerUp}
-            onLayerDown={onLayerDown}
-            onLayerDrag={onLayerDrag}
-        />
+function collect(connect, monitor) {
+    return {
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver()
+    }
+}
+
+const LayerSubtitleSpace = ({ layer, id, onLayerClick, onLayerUp, onLayerDown, onLayerDrag , connectDropTarget, isOver}) => {
+    function over() {
+        if (isOver){
+            return {
+                zIndex: 1,
+                opacity: 0.5,
+                backgroundColor: 'yellow',
+            }
+        }
+        return {}
+    }
+
+    return connectDropTarget(
+        <div key={id} style={over()}>
+            <LayerSubtitle
+                layer={layer}
+                key={id}
+                onLayerClick={onLayerClick}
+                onLayerUp={onLayerUp}
+                onLayerDown={onLayerDown}
+                onLayerDrag={onLayerDrag}
+            />
+        </div>
     )
 }
 
-export default LayerSubtitleSpace
+export default DropTarget("layerSubtitle", subtitleTarget, collect)(LayerSubtitleSpace)
