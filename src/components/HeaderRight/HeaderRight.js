7d30fa8254405a299de93040bee93b6ba8a71884
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-07-03 19:15:36 -0300

    Button style

--- src/components/HeaderRight/HeaderRight.js
+++ src/components/HeaderRight/HeaderRight.js
@@ -3,10 +3,7 @@ import React from 'react'
 const HeaderRight = ({onHeaderClick}) => {
     return (
         <div className="header-right">
-            <div className="header-right--logo-placeholder">
-                <img src={require('../../assets/img/logo.png')} alt="InLoco" className="header-right--logo"/>
-            </div>
-            <a className="header-right--menu-button fa fa-bars" role="button" onClick={onHeaderClick}></a>
+            <a className="header-right--menu-button fa fa-list-ul" role="button" onClick={onHeaderClick}></a>
         </div>
     )
 }
