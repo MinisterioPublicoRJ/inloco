f331b1e9c5762c4909ee95a82291b684865589d9
Barbier <gabrielnasb@gmail.com>
2017-07-11 18:56:54 -0300

    Removing semicolons

--- src/components/LeafletMap/LeafletMapContainer.js
+++ src/components/LeafletMap/LeafletMapContainer.js
@@ -34,18 +34,18 @@ const mapDispatchToProps = (dispatch) => {
         //
         handleMapClick: (e, layers) => {
             const map = e.target
-            const BBOX = map.getBounds().toBBoxString();
-            const WIDTH = map.getSize().x;
-            const HEIGHT = map.getSize().y;
-            const X = map.layerPointToContainerPoint(e.layerPoint).x;
-            const Y = map.layerPointToContainerPoint(e.layerPoint).y;
+            const BBOX = map.getBounds().toBBoxString()
+            const WIDTH = map.getSize().x
+            const HEIGHT = map.getSize().y
+            const X = map.layerPointToContainerPoint(e.layerPoint).x
+            const Y = map.layerPointToContainerPoint(e.layerPoint).y
             layers.map(e => {
                 let url = `?LAYERS=${e.layerName}&QUERY_LAYERS=${e.layerName}&STYLES=,&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&BBOX=${BBOX}&FEATURE_COUNT=3&HEIGHT=${HEIGHT}&WIDTH=${WIDTH}&FORMAT=image%2Fpng&INFO_FORMAT=application%2Fjson&SRS=EPSG%3A4326&X=${X}&Y=${Y}&CQL_FILTER=1%3D1%3B1%3D1`
                 GeoAPI.getLayerData(onUpdateWithSelectedLayerData,url)
             })
         },
     }
-};
+}
 
 const LeafletMapContainer = connect(
     mapStateToProps,
