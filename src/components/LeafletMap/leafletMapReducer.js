ea37b223037de6f1570d4de84cab182c0d7e940b
Barbier <gabrielnasb@gmail.com>
2017-07-11 18:57:32 -0300

    Deleting unused file

--- src/components/LeafletMap/leafletMapReducer.js
+++
@@ -1,9 +0,0 @@
-const mapReducer = (layer) => {
-
-    // let url = `http://apps.mprj.mp.br/geoserver/plataforma/wms?LAYERS=plataforma%3Aamb_drm_cgui,plataforma%3Aretangulo&QUERY_LAYERS=plataforma%3Aamb_drm_cgui,plataforma%3Aretangulo&STYLES=,&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetFeatureInfo&BBOX=-5905256.410373%2C-2879129.171707%2C-3670849.199852%2C-2161232.602153&FEATURE_COUNT=10&HEIGHT=587&WIDTH=1827&FORMAT=image%2Fpng&INFO_FORMAT=text%2Fhtml&SRS=EPSG%3A3857&X=996&Y=199&CQL_FILTER=1%3D1%3B1%3D1`;
-
-    // switch(action.type) {
-    //     case 'GET_LAYER_DATA':
-
-    // }
-}
