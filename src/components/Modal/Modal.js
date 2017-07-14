b78b55fa0ae04cff281ee50bf0dedc267ec7bab2
Barbier <gabrielnasb@gmail.com>
2017-07-14 18:20:30 -0300

    Add list of tables

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,10 +1,29 @@
 import React from 'react'
+import DataTable from '../DataTable/DataTable.js'
 
 const Modal = () => {
     return (
-        <div className="modal">
-            <h1>Hello World!</h1>
-        </div>
+        <section className="modal">
+            <h1 className="modal--title">Tabela de registros</h1>
+            <ul className="modal-layer-list">
+                <li className="modal-layer-list--item">
+                    <a role="button" className="modal-layer-list--link active">Escolas (BASE PARCIAL)</a>
+                </li>
+                <li className="modal-layer-list--item">
+                    <a role="button" className="modal-layer-list--link">Escolas (BASE PARCIAL)</a>
+                </li>
+                <li className="modal-layer-list--item">
+                    <a role="button" className="modal-layer-list--link">Escolas (BASE PARCIAL)</a>
+                </li>
+                <li className="modal-layer-list--item">
+                    <a role="button" className="modal-layer-list--link">Escolas (BASE PARCIAL)</a>
+                </li>
+            </ul>
+            <div className="">Table of contents</div>
+            <ul className="pagination">
+
+            </ul>
+        </section>
     )
 }
 
