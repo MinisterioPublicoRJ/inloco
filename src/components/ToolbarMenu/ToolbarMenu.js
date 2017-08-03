db4c01101851c5868d613b5309472a591ff10f1e
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-08-03 16:10:48 -0300

    Prevent normal menu to be created for search polygon feature

--- src/components/ToolbarMenu/ToolbarMenu.js
+++ src/components/ToolbarMenu/ToolbarMenu.js
@@ -7,7 +7,7 @@ const ToolbarMenu = ({ item, active, type }) => {
         className += " map"
     }
 
-    if((!active || active !== item.name) || item.name === "draw" ) {
+    if((!active || active !== item.name) || item.name === "draw" || item.name === "polygonRequest" ) {
         className += " hidden"
     }
 
