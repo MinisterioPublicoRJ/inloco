9e5016c451ec89f649bfad9aeb6e4755373a7a67
Barbier <gabrielnasb@gmail.com>
2017-08-03 17:57:04 -0300

    Functional method to export map on CSV, XLSX and ShapeFile extensions

--- src/components/ExportList/ExportList.js
+++ src/components/ExportList/ExportList.js
@@ -1,14 +1,15 @@
 import React from 'react'
 
 const ExportList = ({layers}) => {
-    function exportMapData(layers) {
-        let link = document.createElement('a')
-        let url = ''
-
+    function exportMapData(layers, format) {
         layers.filter(layer => {
             if (layer.selected) {
-                url = `http://apps.mprj.mp.br/geoserver/plataforma/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=${layer.name}&outputFormat=excel2007&CQL_FILTER=BBOX(geom,${layer.bbox})`
+                let link = document.createElement('a')
+                let url = `http://apps.mprj.mp.br/geoserver/plataforma/ows?service=WFS&version=2.0&request=GetFeature&typeName=${layer.layerName}&SRSNAME=EPSG:4326&outputFormat=${format}&CQL_FILTER=(BBOX(geom,${layer.bbox},%27EPSG:4326%27))`
+                console.log(layer.bbox)
+                console.log(url)
                 link.setAttribute('href', url);
+                link.setAttribute('download', `${layer.name}.xlsx`);
                 link.click();
             }
         })
@@ -16,16 +17,16 @@ const ExportList = ({layers}) => {
     return (
         <ul className="export-list">
             <li>
-                <a className="export-list--link" role="button" onClick={() => exportMapData(layers)}>Planilha (csv)</a>
+                <a className="export-list--link" role="button" onClick={() => exportMapData(layers, "csv")}>Planilha (csv)</a>
             </li>
             <li>
-                <a className="export-list--link" role="button" onClick={() => exportMapData(layers)}>Planilha (xlsx)</a>
+                <a className="export-list--link" role="button" onClick={() => exportMapData(layers, "excel2007")}>Planilha (xlsx)</a>
             </li>
             <li>
-                <a className="export-list--link" role="button" onClick={() => exportMapData(layers)}>Google Earth (kml)</a>
+                <a className="export-list--link" role="button" onClick={() => exportMapData(layers, "application%5Fvnd.google-earth.kml%2Bxml")}>Google Earth (kml)</a>
             </li>
             <li>
-                <a className="export-list--link" role="button" onClick={() => exportMapData(layers)}>Shape File</a>
+                <a className="export-list--link" role="button" onClick={() => exportMapData(layers, "SHAPE-ZIP")}>Shape File</a>
             </li>
         </ul>
     )
