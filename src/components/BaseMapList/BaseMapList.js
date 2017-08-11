bc20cc6cd2435c9a3cd5de50d1b9d9ccd1ac2cf1
Barbier <gabrielnasb@gmail.com>
2017-08-11 17:36:13 -0300

    Create Basemap List component

--- src/components/BaseMapList/BaseMapList.js
+++ src/components/BaseMapList/BaseMapList.js
@@ -1,10 +1,27 @@
 import React from 'react'
 
-const BaseMapList = () => {
+const BaseMapList = ({baseMaps, onChangeActiveBaseMap}) => {
+
+    function handleChangeActiveBaseMap(baseMap) {
+        return onChangeActiveBaseMap(baseMap)
+    }
+
     return (
         <ul className="basemap-list">
-            <li className="basemap-list--item">OSM</li>
-            <li className="basemap-list--item">Mapbox</li>
+            {
+                // Checks if baseMaps already exists in state
+                baseMaps
+                ? baseMaps.map((baseMap, index) => {
+                    return (
+                        <li className="basemap-list--item" key={index}>
+                            <a role="button" onClick={()=>handleChangeActiveBaseMap(baseMap)}>
+                                <img src={baseMap.image} className="basemap-list--image" alt={`${baseMap.name} base map`}/>
+                            </a>
+                        </li>
+                    )
+                })
+                : ''
+            }
         </ul>
     )
 }
