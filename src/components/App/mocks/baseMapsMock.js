ffafdc933cd8094bb3ce90ad9337a806c598c136
Barbier <gabrielnasb@gmail.com>
2017-08-23 19:16:34 -0300

    Add Google Maps layers and change basemaps. Paired with @nighto, @rlage and @lucianobarauna

--- src/components/App/mocks/baseMapsMock.js
+++ src/components/App/mocks/baseMapsMock.js
@@ -1,12 +1,22 @@
 const BASE_MAPS_MOCK = [
     {
+        name: 'gmaps-roads',
+        image: require('../../../assets/img/basemap-osm.png'),
+    },
+    {
+        name: 'gmaps-terrain',
+        image: require('../../../assets/img/basemap-osm.png'),
+    },
+    {
+        name: 'gmaps-satellite',
+        image: require('../../../assets/img/basemap-osm.png'),
+    },
+    {
         name: 'OSM',
-        url: 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
         image: require('../../../assets/img/basemap-osm.png'),
     },
     {
         name: 'Mapbox Light',
-        url: 'https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiYXJsaW5kbyIsImEiOiJjaWljZDgwemYwMGFydWJrc2FlNW05ZjczIn0.rOROEuNNxKWUIcj6Uh4Xzg',
         image: require('../../../assets/img/basemap-mapbox-light.png'),
     },
 ]
--- src/components/LeafletMap/LeafletMap.js
+++ src/components/LeafletMap/LeafletMap.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import Leaflet from 'leaflet'
-import { Map, WMSTileLayer, TileLayer, Marker, Popup, ZoomControl, ScaleControl, FeatureGroup, Circle } from 'react-leaflet'
-import { EditControl } from "react-leaflet-draw"
+import { Map, WMSTileLayer, TileLayer, Marker, Popup, ZoomControl, ScaleControl, FeatureGroup, Circle, LayersControl } from 'react-leaflet'
+import { EditControl } from 'react-leaflet-draw'
+import { GoogleLayer } from 'react-leaflet-google'
+
+const { BaseLayer, Overlay } = LayersControl
+const key = 'AIzaSyBoZlEM3ASki3UzBfSHpQWW6dM0hHD0no0'
+const terrain = 'TERRAIN'
+const road = 'ROADMAP'
+const satellite = 'SATELLITE'
 
 // Arlindo's token
 const MAPBOX_TOKEN = 'pk.eyJ1IjoiYXJsaW5kbyIsImEiOiJjaWljZDgwemYwMGFydWJrc2FlNW05ZjczIn0.rOROEuNNxKWUIcj6Uh4Xzg'
@@ -16,6 +23,31 @@ require('leaflet/dist/leaflet.css')
 Leaflet.Icon.Default.imagePath = '//cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.0/images/'
 
 const LeafletMap = ({ mapProperties, showMenu, showSidebarRight, layers, showDrawControls, orderByLayerOrder, handleMapClick }) => {
+
+    const availableBasemaps = ['gmaps-roads', 'gmaps-terrain', 'gmaps-satellite', 'OSM', 'Mapbox Light']
+
+
+
+    if (mapProperties && mapProperties.currentMap) {
+
+        let basemapIndex
+        let basemapFormElements = document.querySelectorAll('.leaflet-control-layers-selector')
+        for (var i=0, l=availableBasemaps.length; i<l; i++) {
+            if (mapProperties.currentMap.name === availableBasemaps[i] ) {
+                basemapIndex = i
+            }
+        }
+        // trigger desired basemap
+        basemapFormElements[basemapIndex].click()
+
+        // trigger background layer (last checkbox) twice to put it on the top
+        basemapFormElements[basemapFormElements.length-1].click()
+        basemapFormElements[basemapFormElements.length-1].click()
+    }
+
+
+
+
 
     // basemap
     const currentBaseMap = mapProperties ? mapProperties.currentMap.url : BASEMAP_URL.OPENSTREETMAP
@@ -39,24 +71,38 @@ const LeafletMap = ({ mapProperties, showMenu, showSidebarRight, layers, showDra
     const myHandleMapClick = (e) => {
         handleMapClick(e, layers)
     }
+
+    let MEU_BASE_MAP_TEMPORARIO = 'gmaps'
+
     return (
         <div className={leafletMapClassName}>
             <Map center={position} zoom={zoom} zoomControl={false} onClick={myHandleMapClick}>
 
-                {/*base layer OSM*/}
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url={currentBaseMap}
-                />
+                {/*base map*/}
+                {
+                    MEU_BASE_MAP_TEMPORARIO === 'mapbox' ?
+                        <TileLayer url={BASEMAP_URL.MAPBOX_LIGHT} />
+                    : ''
+                }
+                {
+                    MEU_BASE_MAP_TEMPORARIO === 'osm' ?
+                        <TileLayer url={BASEMAP_URL.OPENSTREETMAP} />
+                    : ''
+                }
+                {/*<LayersControl position='bottomleft'>
+                    <BaseLayer checked={true} name='Google Maps Roads'>
+                        <GoogleLayer googlekey={key}  maptype={road}/>
+                    </BaseLayer>
+                </LayersControl>*/}
 
-                {/*state highlight layer*/}
+                {/*state highlight layer
                 <WMSTileLayer
                     url={ENDPOINT}
                     layers={"plataforma:retangulo"}
                     styles={"plataforma:retangulo"}
                     format={IMAGE_FORMAT}
                     transparent={true}
-                />
+                />*/}
 
                 {/*active layers*/}
                 {orderByLayerOrder(layers).map((layer, index) => {
@@ -66,18 +112,39 @@ const LeafletMap = ({ mapProperties, showMenu, showSidebarRight, layers, showDra
                             layers={layer.layerName}
                             styles={layer.styles[layer.selectedLayerStyleId].name}
                             format={IMAGE_FORMAT}
-                            key={index}
                             transparent={true}
+                            key={index}
                         />
                     )
                 })}
 
-                {/*DEBUG*/}
-                {/*<Marker position={position}>
-                    <Popup>
-                        <span>Hello world</span>
-                    </Popup>
-                </Marker>*/}
+                <LayersControl position='bottomleft'>
+                    <BaseLayer checked={false} name='Google Maps Roads'>
+                        <GoogleLayer googlekey={key}  maptype={road}/>
+                    </BaseLayer>
+                    <BaseLayer checked={false} name='Google Maps Terrain'>
+                        <GoogleLayer googlekey={key}  maptype={terrain} />
+                    </BaseLayer>
+                    <BaseLayer checked={false} name='Google Maps Satellite'>
+                        <GoogleLayer googlekey={key}  maptype={satellite} />
+                    </BaseLayer>
+                    <BaseLayer checked={false} name='OpenStreetMap'>
+                        <TileLayer url={BASEMAP_URL.OPENSTREETMAP} />
+                    </BaseLayer>
+                    <BaseLayer checked={true} name='OpenStreetMap Mapbox Light'>
+                        <TileLayer url={BASEMAP_URL.MAPBOX_LIGHT} />
+                    </BaseLayer>
+                    <Overlay checked={true} name='fundo' className='bla'>
+                        {/*state highlight layer*/}
+                        <WMSTileLayer
+                            url={ENDPOINT}
+                            layers={"plataforma:retangulo"}
+                            styles={"plataforma:retangulo"}
+                            format={IMAGE_FORMAT}
+                            transparent={true}
+                        />
+                    </Overlay>
+                </LayersControl>
 
                 {/*Other controls*/}
                 <ScaleControl position="bottomleft"/>
