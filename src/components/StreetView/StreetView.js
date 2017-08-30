aafe07e18703634d900b7f75a762f2187c29781f
Luciano Baraúna <luciano.lourenco@mprj.mp.br>
2017-08-30 17:43:43 -0300

    implementing modal street-view

--- src/components/StreetView/StreetView.js
+++ src/components/StreetView/StreetView.js
@@ -7,8 +7,11 @@ const StreetView = ({ streetViewCoordinates }) => {
     let GOOGLE_STREET_VIEW_URL = `https://www.google.com/maps/embed/v1/streetview?key=AIzaSyBoZlEM3ASki3UzBfSHpQWW6dM0hHD0no0&location=${streetViewCoordinates.lat},${streetViewCoordinates.lng}`
 
     return (
-        <div className="streetViewContainer">
-            <iframe id="streetView" src={GOOGLE_STREET_VIEW_URL} frameBorder="0" allowFullScreen></iframe>
+        <div className="modal-streetView">
+            <button className="close">Fechar</button>
+            <div className="streetViewContainer">
+                <iframe id="streetView" src={GOOGLE_STREET_VIEW_URL} frameBorder="0" allowFullScreen></iframe>
+            </div>
         </div>
     )
 }
