859743780fa1ed54141f759f15b5beb469150973
Luciano Baraúna <luciano.lourenco@mprj.mp.br>
2017-08-30 17:52:58 -0300

    add streetView.scss

--- src/components/StreetView/StreetView.js
+++ src/components/StreetView/StreetView.js
@@ -7,9 +7,11 @@ const StreetView = ({ streetViewCoordinates, onStreetViewHide }) => {
     let GOOGLE_STREET_VIEW_URL = `https://www.google.com/maps/embed/v1/streetview?key=AIzaSyBoZlEM3ASki3UzBfSHpQWW6dM0hHD0no0&location=${streetViewCoordinates.lat},${streetViewCoordinates.lng}`
 
     return (
-        <div className="streetViewContainer">
-            <div className="streetViewContainer-closeButton" onClick={onStreetViewHide}>X</div>
-            <iframe id="streetView" src={GOOGLE_STREET_VIEW_URL} frameBorder="0" allowFullScreen></iframe>
+        <div className="streetview-modal">
+            <div className="streetViewContainer">
+                <div className="streetViewContainer-closeButton" onClick={onStreetViewHide}>X</div>
+                <iframe id="streetView" src={GOOGLE_STREET_VIEW_URL} frameBorder="0" allowFullScreen></iframe>
+            </div>
         </div>
     )
 }
