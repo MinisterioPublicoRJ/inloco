6eecc0477778bacb035ba86fc5ffcfcaef56275a
Luciano Baraúna <luciano.lourenco@mprj.mp.br>
2017-09-01 11:55:14 -0300

    update style global and markup

--- src/components/GlobalFilter/GlobalFilter.js
+++ src/components/GlobalFilter/GlobalFilter.js
@@ -23,43 +23,53 @@ const GlobalFilter = ({
 
     return (
         <div className="global-filter-container">
-            <span> Busca detalhada </span>
-            <input
-                type="search"
-                placeholder="Insira o nome da área"
-                ref={node => {input = node;}}
-                onKeyUp={() => {
-                    handleKeyUpSearch(input.value)
-                }}
-            />
-            <span> Tipo de seleção </span>
             <form>
-                <div className="selectionTypeContainer">
-                    <label htmlFor="selectionType1" className="asdasd">
+                <fieldset className="global-filterform">
+                    <p className="global-filter-container--title">Busca detalhada</p>
+                    <fieldset className="global-filterform--inpusearch">
                         <input
-                            name="selectionType"
-                            type="radio"
-                            id="selectionType1"
-                            value="borda"
-                            defaultChecked="checked"
-                            onClick={(e) => handleTypeChange(e)}/>
-                        Demarcada
-                    </label>
-                    <label htmlFor="selectionType2" className="asdasd">
-                        <input
-                            name="selectionType"
-                            type="radio"
-                            id="selectionType2"
-                            value="opaco"
-                            onClick={(e) => handleTypeChange(e)}/>
-                        Isolada
-                    </label>
-                </div>
-                <label> Opacidade da seleção
-                    <input className="opacitySelection" type="range" min="0" max="10" defaultValue="5" onChange={(e) => handleOpacityChange(e)}></input>
-                </label>
+                            id="searchField"
+                            type="search"
+                            placeholder="Insira o nome da área"
+                            ref={node => {input = node;}}
+                            onKeyUp={() => {
+                                handleKeyUpSearch(input.value)
+                            }}
+                        />
+
+                        <label htmlFor="searchField"><i className="fa fa-search"></i></label>
+                    </fieldset>
+                    <fieldset className="global-filterform--selectiontype">
+                        <label>Tipo de seleção</label>
+                        <label htmlFor="selectionType1" className="asdasd">
+                            <input
+                                name="selectionType"
+                                type="radio"
+                                id="selectionType1"
+                                value="borda"
+                                defaultChecked="checked"
+                                onClick={(e) => handleTypeChange(e)} />
+                            Demarcada
+                        </label>
+                        <label htmlFor="selectionType2" className="asdasd">
+                            <input
+                                name="selectionType"
+                                type="radio"
+                                id="selectionType2"
+                                value="opaco"
+                                onClick={(e) => handleTypeChange(e)} />
+                            Isolada
+                        </label>
+                    </fieldset>
+
+                    <fieldset className="global-filterform--selectopacity">
+                        <label> Opacidade da seleção
+                            <input className="opacitySelection" type="range" min="0" max="10" defaultValue="5" onChange={(e) => handleOpacityChange(e)}></input>
+                        </label>
+                    </fieldset>
+                </fieldset>
             </form>
-            <span> Áreas do Rio de Janeiro </span>
+            <p>Áreas do Rio de Janeiro</p>
             <div className="places">
                 {places? places.map(p => <Place onPlaceClick={onPlaceClick} key={p.id} place={p}/>) : null}
             </div>
