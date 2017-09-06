af4a5a055d5cce8cb34a03d483cc566098773c5e
Barbier <gabrielnasb@gmail.com>
2017-09-06 13:03:34 -0300

    Remove border from last item

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -79,16 +79,28 @@ const Modal = ({ showModal, layers, lastClickData, newsModal, onCloseModal, onGe
                 </h1>
                 <dl className="news-modal-list">
                     <dt className="news-modal-list--title">Nova Funcionalidade – Desenho Livre</dt>
-                    <dd className="news-modal-list--description">Com esta nova funcionalidade, o usuário pode desenhar e anotar em cima das camadas de exibição facilitando assim a inserção em documento.</dd>
+                    <dd className="news-modal-list--description">
+                        Com esta nova funcionalidade, o usuário pode desenhar e anotar em cima das camadas de exibição facilitando assim a inserção em documento.
+                        <img src={require('../../assets/img/news/funcionalidade.png')} alt="" className="news-modal-list--image"/>
+                    </dd>
 
                     <dt className="news-modal-list--title">Nova Camada – “Escolas”</dt>
-                    <dd className="news-modal-list--description">Escolas federais, estaduais, municipais e privadas do Estado do Rio de Janeiro. Fonte: Censo escolar da educação básica 2015 (Ministério da Educação/INEP)</dd>
+                    <dd className="news-modal-list--description">
+                        Escolas federais, estaduais, municipais e privadas do Estado do Rio de Janeiro. Fonte: Censo escolar da educação básica 2015 (Ministério da Educação/INEP)
+                        <img src={require('../../assets/img/news/escolas.png')} alt="" className="news-modal-list--image"/>
+                    </dd>
 
                     <dt className="news-modal-list--title">Nova Camada – “Comunidades”</dt>
-                    <dd className="news-modal-list--description">Limites das Comunidades do Estado do Rio de Janeiro - Fonte: SABREN/IPP - 2014 e Setores de Aglomerados Subnormais do IBGE - Censo 2010.</dd>
+                    <dd className="news-modal-list--description">
+                        Limites das Comunidades do Estado do Rio de Janeiro - Fonte: SABREN/IPP - 2014 e Setores de Aglomerados Subnormais do IBGE - Censo 2010.
+                        <img src={require('../../assets/img/news/favelas.png')} alt="" className="news-modal-list--image"/>
+                    </dd>
 
                     <dt className="news-modal-list--title">Nova Camada – “Geologia”</dt>
-                    <dd className="news-modal-list--description">Litologia do Estado do Rio de Janeiro na escala 1:50000</dd>
+                    <dd className="news-modal-list--description">
+                        Litologia do Estado do Rio de Janeiro na escala 1:50000
+                        <img src={require('../../assets/img/news/geologia.png')} alt="" className="news-modal-list--image"/>
+                    </dd>
                 </dl>
 
             </section>
