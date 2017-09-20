25e28a2eb23cef00623632a0ae6a2cc35c42f984
Barbier <gabrielnasb@gmail.com>
2017-09-20 14:33:39 -0300

    Change hide news modal method

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,9 @@ const Header = ({onHeaderClick, showTooltipMenu}) => {
     return (
         <div className="application-header">
             <div className="application-header--logo-placeholder">
+            <a href="http://apps.mprj.mp.br/sistema/mpmapas/home.html" target="_blank">
                 <img src={require('../../assets/img/logo.png')} alt="InLoco" className="application-header--logo"/>
+            </a>
             </div>
             <a className="application-header--menu-button fa fa-bars focus-menu" role="button" onClick={onHeaderClick}>
                 <span className={className}>Comece por aqui.</span>
