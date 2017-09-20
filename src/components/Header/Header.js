1d9b4c854a724e4c001e2bb243a08ddf7b7baac6
Barbier <gabrielnasb@gmail.com>
2017-09-20 14:07:55 -0300

    Change texts

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,7 +11,7 @@ const Header = ({onHeaderClick, showTooltipMenu}) => {
                 <img src={require('../../assets/img/logo.png')} alt="InLoco" className="application-header--logo"/>
             </div>
             <a className="application-header--menu-button fa fa-bars focus-menu" role="button" onClick={onHeaderClick}>
-                <span className={className}>Começe por aqui.</span>
+                <span className={className}>Comece por aqui.</span>
             </a>
         </div>
     )
