0d06493b84c4363abcba9c2993174d524559f68d
Arlindo Pereira <arlindosaraivapereira@gmail.com>
2017-09-21 15:52:27 -0300

    Adding opengraph meta tags and favicons

--- copy_index.js
+++
@@ -1,5 +0,0 @@
-var cpx = require('cpx')
-
-console.log('Copying Index file...')
-cpx.copySync('public/index.html', 'static/')
-console.log('OK')
---
+++ copy_static.js
@@ -0,0 +1,11 @@
+var cpx = require('cpx')
+
+console.log('Copying static files...')
+cpx.copySync('public/index.html', 'static/')
+cpx.copySync('public/favicon.ico', 'static/')
+cpx.copySync('public/icon-apple-touch-57x57.jpg', 'static/')
+cpx.copySync('public/icon-apple-touch-72x72.jpg', 'static/')
+cpx.copySync('public/icon-apple-touch-114x114.jpg', 'static/')
+cpx.copySync('public/icon-apple-touch-144x144.jpg', 'static/')
+cpx.copySync('public/og-img.jpg', 'static/')
+console.log('OK')
