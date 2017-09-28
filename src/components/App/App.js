bc04d8185f104de8233340b02d89b4b7aee35cdb
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-09-28 09:33:19 -0300

    Adding google analytics. Fixes #310

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import ReactGA from 'react-ga'
 import logger from 'redux-logger'
 import { applyMiddleware, createStore } from 'redux'
 import appReducer from './appReducer.js'
@@ -17,6 +18,13 @@ import ModalContainer from '../Modal/ModalContainer.js'
 import LoadingContainer from '../Loading/LoadingContainer.js'
 
 require('./app.scss')
+
+ReactGA.initialize('UA-80844385-5');
+
+function logPageView() {
+    ReactGA.set({ page: window.location.pathname + window.location.search });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+}
 
 // start by removing pre-loading
 document.getElementById('pre-loading').remove()
@@ -102,3 +110,5 @@ const App = () => {
 ReactDOM.render(
     <App/>, document.getElementById('app')
 )
+
+logPageView()
