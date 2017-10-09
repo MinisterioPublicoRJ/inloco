2763e814ce1cdb519c7e191694a7c0b8b98502be
rafael.tavares <rafael.tavares@mprj.mp.br>
2017-10-09 18:34:50 -0300

    Creating login component

---
+++ src/components/Modal/Contents/Login.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Login = () => {
+
+    return (
+        <div className="about-container">
+            <p>O inLoco 2.0 é uma plataforma de mapas interativos criada pelo <a href="http://www.mprj.mp.br/" target="_blank">Ministério Público do Estado do Rio de Janeiro</a>, permitindo ao usuário visualizar e sobrepor dados geográficos de diversos assuntos, realizar buscas e dispor de diversas informações.</p>
+            <p>Em caso de dúvidas na utilização do sistema, mande um email para <a href="mailto:mpemmapas.cadg@mprj.mp.br">mpemmapas.cadg@mprj.mp.br</a> ou ligue para <a href="tel:+552122621326">(21) 2262-1326</a>.</p>
+            <p>Este sistema é software livre e seu código está disponibilizado no <a href="https://github.com/MinisterioPublicoRJ/inLoco-2.0">GitHub</a>. Contribuições são bem-vindas! :)</p>
+        </div>
+    )
+}
+
+export default Login
