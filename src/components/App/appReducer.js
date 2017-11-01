eca47fb630f673124272c6fe06c32abfedb59f1f
rlage <rafael.tavares@mprj.mp.br>
2017-11-01 11:07:19 -0200

    remove console log

--- src/components/App/appReducer.js
+++ src/components/App/appReducer.js
@@ -41,7 +41,6 @@ const searchPlaceById = (place, id) => {
 var resultPlaces = []
 const searchPlaceByTitle = (place, text) => {
     if (place.title.toLowerCase().includes(text.toLowerCase()) && place.id !== ESTADO_ID && text !== "") {
-        console.log(place.title)
         place.show = true
     } else if (place.id !== ESTADO_ID && place.tipo !== CRAAI) {
         place.show = false
