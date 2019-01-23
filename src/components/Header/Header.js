54dd69f572d284cacf2acd6455ba5450d2097af1
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2019-01-23 16:42:34 -0200

    changing logo and colors

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,9 +8,7 @@ const Header = ({onHeaderClick, showTooltipMenu}) => {
     return (
         <div className="application-header">
             <div className="application-header--logo-placeholder">
-            <a href="http://mpemmapas.mprj.mp.br/home.html" target="_blank">
-                <img src={require('../../assets/img/logo-rj.png')} alt="InLoco" className="application-header--logo"/>
-            </a>
+            <img src={require('../../assets/img/rj-inloco.png')} alt="InLoco" className="application-header--logo"/>
             </div>
             <a className="application-header--menu-button fa fa-bars focus-menu" role="button" onClick={onHeaderClick} data-html2canvas-ignore={true}>
                 <span className={className}>Comece por aqui.</span>
