46f6db3b22210457e68d46518d961b2aaca73e98
Arlindo Pereira <arlindo.pereira@mprj.mp.br>
2019-01-23 15:45:41 -0200

    Changing logo to Rio state

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -9,7 +9,7 @@ const Header = ({onHeaderClick, showTooltipMenu}) => {
         <div className="application-header">
             <div className="application-header--logo-placeholder">
             <a href="http://mpemmapas.mprj.mp.br/home.html" target="_blank">
-                <img src={require('../../assets/img/logo.png')} alt="InLoco" className="application-header--logo"/>
+                <img src={require('../../assets/img/logo-rj.png')} alt="InLoco" className="application-header--logo"/>
             </a>
             </div>
             <a className="application-header--menu-button fa fa-bars focus-menu" role="button" onClick={onHeaderClick} data-html2canvas-ignore={true}>
--- src/components/MenuHeader/MenuHeader.js
+++ src/components/MenuHeader/MenuHeader.js
@@ -3,7 +3,7 @@ import React from 'react'
 const MenuHeader = ({onClickMenuHeader}) => {
     return (
         <div className="menu-header">
-            <img src={require('../../assets/img/logo.png')} alt="InLoco" className="menu-header--logo"/>
+            <img src={require('../../assets/img/logo-rj.png')} alt="InLoco" className="menu-header--logo"/>
             <a role="button" className="menu-header--button fa fa-times" onClick={onClickMenuHeader}></a>
             <h1 className="menu-header--title">
                 Camadas de Exibição
